b6a45fb77161a8aefb2e68ae2ac76bcb5b5296ec
Federico Gratti <federicogratti@gmail.com>
2020-08-24 01:50:09 -0300

    Ability to set drp on Configuration

--- src/Configuration.js
+++ src/Configuration.js
@@ -4,10 +4,11 @@
 
 */
 
-class Configuration {
-  constructor(parameters = {}) {
-
-    this.dpr = window.devicePixelRatio ? window.devicePixelRatio : 1;
+class Configuration
+{
+  constructor(parameters = {})
+  {
+    this.dpr = 1;
     this.is_mobile = false;
     this.is_ios = false;
     this.is_ipad = false;
@@ -20,19 +21,12 @@ class Configuration {
     this.max_zoom_distance_scale = 1.1;
     this.zoom_speed = 0.1;
 
-
-
     this.use_fxaa = false;
     this.use_ssaa = true;
 
+    this.background_color = new THREE.Color(0.768, 0.768, 0.768);
 
-
-    this.background_color = new THREE.Color(0.768,0.768,0.768);
-
-
-
-
-    //TOUCH
+    // TOUCH
     this.touch_sensitivity = 1.5;
     this.zoom_sensitivity = 0.7;
     this.spin_sensitivity = 2;
@@ -40,9 +34,7 @@ class Configuration {
     this.three_finger_allowed_x_rotation_offset = 30;
     this.three_finger_allowed_y_rotation_offset = 20;
 
-
     this.use_touch_gestures = false;
-
 
     this.texture_float_supported = false;
 
@@ -57,27 +49,17 @@ class Configuration {
     this.show_debug_sphere_on_steps_in_floor = false;
     this.display_cleanup_log = false;
 
-
     this.navigation_speed_damping = 0.74;
     this.navigation_speed = 200;
     this.out_of_bounds_force = 1.5;
     this.mouse_zoom_speed = 0.05;
     this.touch_zoom_speed = 0.5;
-
-
   }
-
-
-
-
-
 
   from_json(json)
   {
 
-
   }
 }
 
-const configuration = new Configuration();
-module.exports = configuration;
+export default new Configuration();
--- src/Graphics.js
+++ src/Graphics.js
@@ -1,6 +1,6 @@
-import Configuration from           '/Configuration';
-import BaseRender from   '/render_mode/BaseRender';
-import OutlineRender from           '/render_mode/OutlineRender';
+import Configuration from '/Configuration';
+import BaseRender from '/render_mode/BaseRender';
+import OutlineRender from '/render_mode/OutlineRender';
 import Screen from '/Screen';
 import CameraManager from '/CameraManager';
 import SceneManager from '/SceneManager';
@@ -8,10 +8,10 @@ import Capabilities from '/Capabilities';
 import DepthAndNormalsRenderer from '/render_utilities/DepthAndNormalsRenderer';
 import Blitter from '/render_utilities/Blitter';
 
-class Graphics {
-
-
-  constructor() {
+class Graphics
+{
+  constructor()
+  {
     this._renderer = undefined;
     this.blitter = undefined;
     this.canvas = undefined;
@@ -25,7 +25,8 @@ class Graphics {
     this.context_attributes = undefined;
   }
 
-  init(canvas, msaa = true, context_attributes) {
+  init(canvas, msaa = true, context_attributes)
+  {
     this.context_attributes = context_attributes || {
       alpha: true,
       depth: true,
@@ -36,7 +37,7 @@ class Graphics {
       preserveDrawingBuffer: true,
       powerPreference: 'high-performance',
       logarithmicDepthBuffer: false
-    }
+    };
 
     this.canvas_context = canvas.getContext('webgl', this.context_attributes) ||
                           canvas.getContext('webgl', this.context_attributes) ||
@@ -54,10 +55,11 @@ class Graphics {
 
     this._renderer.setPixelRatio(1);
 
-    Screen.dpr = window.devicePixelRatio;
+    Screen.dpr = Configuration.dpr;
 
-    if(!this.is_webgl2) {
-      this._renderer.extensions.get('ANGLE_instanced_arrays')
+    if (!this.is_webgl2)
+    {
+      this._renderer.extensions.get('ANGLE_instanced_arrays');
     }
 
     this.blitter = new Blitter(this._renderer);
@@ -75,7 +77,6 @@ class Graphics {
     this.generateDepthNormalTexture = false;
 
     this.depth_and_normals_renderer = new DepthAndNormalsRenderer();
-
   }
 
   get dom_element()
@@ -88,25 +89,25 @@ class Graphics {
     return this.depth_and_normals_renderer.render_target;
   }
 
-
   set_state(new_state)
   {
-    console.log("VIEWAPI - map render mode switch to: " + new_state.constructor.name);
+    console.log('VIEWAPI - map render mode switch to: ' + new_state.constructor.name);
 
     this.current_render_mode.on_exit(this, this._renderer);
     this.current_render_mode = new_state;
     this.current_render_mode.on_enter(this, this._renderer);
   }
 
-  update() {
+  update()
+  {
     this.check_for_resize();
 
-    if(this.generateDepthNormalTexture)
+    if (this.generateDepthNormalTexture)
     {
       this.depth_and_normals_renderer.render(this);
     }
 
-    if(CameraManager.current)
+    if (CameraManager.current)
     {
       this.__update_current_camera();
       this.current_render_mode.render();
@@ -121,61 +122,60 @@ class Graphics {
     CameraManager.current.updateMatrixWorld(true);
   }
 
-
-
   render(scene, camera, RT, override_mat)
   {
     this.__apply_override_material(scene, override_mat);
 
-    this._renderer.setRenderTarget(RT === undefined? null : RT);
+    this._renderer.setRenderTarget(RT === undefined ? null : RT);
     this._renderer.render(scene  || SceneManager.current,
-                          camera || CameraManager.current);
+      camera || CameraManager.current);
 
     this.__apply_override_material(scene, undefined);
-
-
   }
 
   __apply_override_material(scene, mat)
   {
-    if(scene)
+    if (scene)
+    {
       scene.overrideMaterial = mat;
+    }
     else
+    {
       SceneManager.current.overrideMaterial = mat;
+    }
   }
 
   readback_RT(RT, buffer)
   {
-    this._renderer.readRenderTargetPixels( RT, 0, 0, RT.width, RT.height, buffer);
+    this._renderer.readRenderTargetPixels(RT, 0, 0, RT.width, RT.height, buffer);
   }
 
   clear(RT, camera, clear_depth, clear_stencil)
   {
-    this._renderer.setRenderTarget(RT === undefined? null : RT);
+    this._renderer.setRenderTarget(RT === undefined ? null : RT);
 
-    if(camera)
+    if (camera)
     {
       this._renderer.setClearColor(camera.clear_color, camera.clear_alpha);
     }
 
-    this._renderer.clear( camera?         true : false, // clear color
-                          clear_depth?    true : false,
-                          clear_stencil?  true : false);
+    this._renderer.clear(!!camera, // clear color
+      !!clear_depth,
+      !!clear_stencil);
   }
 
   check_for_resize()
   {
-
     let current_width   = this.canvas.clientWidth;
     let current_height  = this.canvas.clientHeight;
 
     if (
-          current_width  !== Screen.width  ||
-          current_height !== Screen.height ||
-          window.devicePixelRatio !== Screen.dpr
-        )
+      current_width  !== Screen.width  ||
+      current_height !== Screen.height ||
+      Configuration.dpr !== Screen.dpr
+    )
     {
-      Screen.dpr = window.devicePixelRatio;
+      Screen.dpr = Configuration.dpr;
       Screen.update_size(current_width, current_height);
 
       this.canvas.width  = Screen.render_width;
@@ -186,17 +186,21 @@ class Graphics {
     }
   }
 
-  on_resize() {
-    console.error("Graphics.on_resize call no longer needed.")
+  on_resize()
+  {
+    console.error('Graphics.on_resize call no longer needed.');
   }
-
 
   blit(src_RT, dst_RT, mat)
   {
-    if(mat)
+    if (mat)
+    {
       this.blitter.blit_with_material(src_RT, dst_RT, mat);
+    }
     else
+    {
       this.blitter.blit(src_RT, dst_RT);
+    }
   }
 
   blit_clear_with_material(dst_RT, mat)
@@ -204,12 +208,9 @@ class Graphics {
     this.blitter.blit_clear_with_material(dst_RT, mat);
   }
 
-
   take_screenshot(blob_callback)
   {
-
     const ctx = this;
-
 
     let old_width = Screen.width;
     let old_height = Screen.height;
@@ -227,7 +228,7 @@ class Graphics {
 
     this._renderer.setPixelRatio(1);
 
-    this._renderer.setSize(tile_width ,tile_height, false);
+    this._renderer.setSize(tile_width, tile_height, false);
 
     this.ctx_2D.canvas.width  = new_width;
     this.ctx_2D.canvas.height = new_height;
@@ -236,54 +237,52 @@ class Graphics {
     CameraManager.current.updateMatrix();
     CameraManager.current.updateMatrixWorld(true);
 
-    for(let x = 0; x < divisions_x; x++)
+    for (let x = 0; x < divisions_x; x++)
     {
-      for(let y = 0; y < divisions_y; y++)
+      for (let y = 0; y < divisions_y; y++)
       {
-
-        CameraManager.current.setViewOffset( new_width,             new_height,
-                                             Screen.width*x,         Screen.height*y,
-                                             Screen.width,            Screen.height);
+        CameraManager.current.setViewOffset(new_width,             new_height,
+          Screen.width * x,         Screen.height * y,
+          Screen.width,            Screen.height);
         this.current_render_mode.render();
 
-        this.ctx_2D.drawImage(this._renderer.domElement, Screen.width*x, Screen.height*y);
+        this.ctx_2D.drawImage(this._renderer.domElement, Screen.width * x, Screen.height * y);
       }
     }
 
     // transform the result canvas into a blob
     // from them the callback turns into a ULR and download it
-    this.ctx_2D.canvas.toBlob(blob_callback,"image/png;base64;");
+    this.ctx_2D.canvas.toBlob(blob_callback, 'image/png;base64;');
 
     CameraManager.current.clearViewOffset();
-    Screen.update_size(old_width,old_height);
-    this._renderer.setPixelRatio(window.devicePixelRatio);
-    this._renderer.setSize(old_width,old_height, false);
+    Screen.update_size(old_width, old_height);
+    this._renderer.setPixelRatio(Configuration.dpr);
+    this._renderer.setSize(old_width, old_height, false);
 
     CameraManager.current.aspect = Screen.aspect_ratio;
     CameraManager.current.updateMatrix();
     CameraManager.current.updateMatrixWorld(true);
-
   }
 
-  download_screenshot (blob) {
-    console.log("el blob", blob);
+  download_screenshot(blob)
+  {
+    console.log('el blob', blob);
     let link = document.createElement('a');
-    link.download = "Snapshot.png";
+    link.download = 'Snapshot.png';
 
     link.href = URL.createObjectURL(blob);
-    link.click()
+    link.click();
 
-    link.onclick = function() {
-      requestAnimationFrame(function() {
+    link.onclick = function()
+    {
+      requestAnimationFrame(function()
+      {
         URL.revokeObjectURL(a.href);
       });
-      a.removeAttribute('href')
+      a.removeAttribute('href');
     };
-
   }
-
 }
 
 const graphics = new Graphics();
 module.exports = graphics;
-
