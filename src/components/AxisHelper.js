3f56ed497636c1b8473615137a85949de954a348
Federico Gratti <federicogratti@gmail.com>
2021-05-17 00:59:07 +0200

    Fix AxisHelper

--- src/components/AxisHelper.js
+++ src/components/AxisHelper.js
@@ -2,7 +2,7 @@
 import { Object3D } from 'three';
 import { LineBasicMaterial } from 'three';
 import { AlwaysDepth } from 'three';
-import { Geometry } from 'three';
+import { BufferGeometry } from 'three';
 import { Vector3 } from 'three';
 import { Line } from 'three';
 
@@ -12,30 +12,39 @@ export default class AxisHelper extends Object3D
   {
     super();
 
-    const blueAxisMat = new LineBasicMaterial({ color: 0x4444ff, depthFunc: AlwaysDepth });
-    const blueAxisGeo = new Geometry();
-    blueAxisGeo.vertices.push(new Vector3(0, 0, 0));
-    blueAxisGeo.vertices.push(new Vector3(0, 0, 1000));
-    const blueAxisLine = new Line(blueAxisGeo, blueAxisMat);
-    blueAxisLine.renderOrder = 50000;
+    const blue_points = [];
+    blue_points.push(new Vector3(0, 0, 0));
+    blue_points.push(new Vector3(0, 0, 1000));
 
-    const greenAxisMat = new LineBasicMaterial({ color: 0x44ff44, depthFunc: AlwaysDepth });
-    const greenAxisGeo = new Geometry();
-    greenAxisGeo.vertices.push(new Vector3(0, 0, 0));
-    greenAxisGeo.vertices.push(new Vector3(0, 1000, 0));
-    const greenAxisLine = new Line(greenAxisGeo, greenAxisMat);
-    greenAxisLine.renderOrder = 50000;
+    const blue_axis_mat = new LineBasicMaterial({ color: 0x4444ff, depthFunc: AlwaysDepth });
+    const blue_axis_geo = new BufferGeometry().setFromPoints(blue_points);
+    const blue_axis_line = new Line(blue_axis_geo, blue_axis_mat);
+    blue_axis_line.renderOrder = 50000;
 
-    const redAxisMat = new LineBasicMaterial({ linewidth: 100, color: 0xff4444, depthFunc: AlwaysDepth });
-    const redAxisGeo = new Geometry();
-    redAxisGeo.vertices.push(new Vector3(0, 0, 0));
-    redAxisGeo.vertices.push(new Vector3(1000, 0, 0));
-    const redAxisLine = new Line(redAxisGeo, redAxisMat);
-    redAxisLine.renderOrder = 50000;
+    const green_points = [];
+    green_points.push(new Vector3(0, 0, 0));
+    green_points.push(new Vector3(0, 1000, 0));
+
+    const green_axis_mat = new LineBasicMaterial({ color: 0x44ff44, depthFunc: AlwaysDepth });
+    const green_axis_geo = new BufferGeometry().setFromPoints(green_points);
+    const green_axis_line = new Line(green_axis_geo, green_axis_mat);
+    green_axis_line.renderOrder = 50000;
+
+    const red_points = [];
+    red_points.push(new Vector3(0, 0, 0));
+    red_points.push(new Vector3(1000, 0, 0));
+
+    const red_axis_mat = new LineBasicMaterial({ color: 0xff4444, depthFunc: AlwaysDepth });
+    const red_axis_geo = new BufferGeometry().setFromPoints(red_points);
+    const red_axis_line = new Line(red_axis_geo, red_axis_mat);
+    red_axis_line.renderOrder = 50000;
+
     this.renderOrder = 100000;
-    this.add(blueAxisLine);
-    this.add(greenAxisLine);
-    this.add(redAxisLine);
+
+    this.add(blue_axis_line);
+    this.add(green_axis_line);
+    this.add(red_axis_line);
+
     this.scale.set(scale, scale, scale);
   }
 
@@ -45,6 +54,5 @@ export default class AxisHelper extends Object3D
 
   dispose()
   {
-
   }
 }
