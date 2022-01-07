a7324b350d5d83a833e88225d86eb1aa7d32b1fc
Federico Gratti <federicogratti@gmail.com>
2022-01-07 10:38:40 -0300

    Improve Debug type definition

--- types/Debug.d.ts
+++ types/Debug.d.ts
@@ -3,6 +3,7 @@ import { AxisHelper } from "ohzi-core/types/components/AxisHelper";
 import { Arrow } from "ohzi-core/types/primitives/Arrow";
 import { Cube } from "ohzi-core/types/primitives/Cube";
 import { Sphere } from "ohzi-core/types/primitives/Sphere";
+import { Vector3 } from "three";
 
 export class Debug {
     static init(): void;
@@ -26,7 +27,7 @@ export class Debug {
     static draw_oriented_cube(from: any, to: any, height?: number, color?: string, depth?: number): Cube;
     static draw_plane(width: any, height: any, color: any): any;
     static draw_empty_cube(pos: any, size: any, color: any): any;
-    static draw_sphere(pos: any, size: any, color: any): Sphere;
+    static draw_sphere(pos: Vector3, size: number, color: string): Sphere;
     static draw_point_array(input_points: any, open?: boolean, color?: number): any;
     static draw_sphere_helper(sphere: any, color: any): any;
     static draw_math_sphere(sphere: any): void;
