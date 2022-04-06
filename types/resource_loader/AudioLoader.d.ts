e93a501e9b96f1991fa495d1a1e3e9ed3b04990b
Federico Gratti <federicogratti@gmail.com>
2022-04-06 15:58:31 -0300

    Add buffer to Time.delta_time

--- types/resource_loader/AudioLoader.d.ts
+++ types/resource_loader/AudioLoader.d.ts
@@ -1,7 +1,7 @@
 import { AbstractLoader } from "./AbstractLoader";
 
 export class AudioLoader extends AbstractLoader {
-    constructor(resource_id: any, url: any, loop: boolean, volume: number, size: any);
+    constructor(resource_id: string, url: string, loop: boolean, volume: number, size: number, positional: boolean);
     loader: any;
     loop: boolean;
     volume: number;
