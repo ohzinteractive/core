5bd56b12dca3cae86bd403c24827590bd3589bbe
Federico Gratti <federicogratti@gmail.com>
2022-04-14 09:34:01 -0300

    Stop using FileLoader to request JSON files

--- src/resource_loader/JSONLoader.js
+++ src/resource_loader/JSONLoader.js
@@ -1,40 +1,20 @@
 import AbstractLoader from './AbstractLoader';
 
-import { FileLoader as THREEFileLoader } from 'three';
-
 export default class JSONLoader extends AbstractLoader
 {
   constructor(resource_id, url, size)
   {
     super(resource_id, url, size);
-
-    this.loader = new THREEFileLoader();
   }
 
-  on_preloaded_finished(resource_container)
+  on_preloaded_finished(resource_container, response)
   {
-    const ctx = this;
+    response.json().then((json) =>
+    {
+      resource_container.set_resource(this.resource_id, this.url, json);
 
-    this.loader.load(this.url, (data) =>
-    {
-      resource_container.set_resource(ctx.resource_id, ctx.url, JSON.parse(data));
-
-      ctx.__update_downloaded_bytes(1, 1);
-      ctx.__loading_ended();
-    },
-    (xhr) =>
-    {
-      // if (xhr)
-      // {
-      //   let total = xhr.total || this.total_bytes;
-      //   ctx.__update_downloaded_bytes(xhr.loaded, total);
-      // }
-    },
-    (msg) =>
-    {
-      ctx.__set_error(msg + '\n\n\t If the error says something about unexpected token < in JSON then the probably the problem is related to the file not being found. Check the name and path of the resource');
-      ctx.__loading_ended();
-    }
-    );
+      this.__update_downloaded_bytes(1, 1);
+      this.__loading_ended();
+    });
   }
 }
