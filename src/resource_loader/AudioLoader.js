38bdd4b9bbd73ff6130a51b1a9fb39346c1e6272
Federico Gratti <federicogratti@gmail.com>
2022-07-03 16:00:17 +0200

    Update AudioLoader

--- src/resource_loader/AudioLoader.js
+++ src/resource_loader/AudioLoader.js
@@ -1,8 +1,7 @@
-import AbstractLoader from './AbstractLoader';
-
-import AudioClip from '../components/AudioClip';
+import { AbstractLoader } from 'ohzi-core';
 
 import { AudioContext } from 'three';
+import AudioClip from './AudioClip';
 
 export default class AudioLoader extends AbstractLoader
 {
@@ -14,44 +13,64 @@ export default class AudioLoader extends AbstractLoader
     this.positional = positional;
   }
 
-  on_preloaded_finished(resource_container, response)
+  on_preloaded_finished(resource_container)
   {
-    if (!window.user_interaction_for_audio)
+    // if (!window.user_interaction_for_audio)
+    // {
+    //   setTimeout(this.on_preloaded_finished.bind(this, resource_container), 100);
+    // }
+    // else
+    // {
+    if (resource_container.resources_by_url[this.url] === undefined)
     {
-      setTimeout(this.on_preloaded_finished.bind(this, resource_container, response), 100);
+      this.instantiate_audio(resource_container);
     }
     else
     {
-      if (resource_container.resources_by_url[this.url] === undefined)
-      {
-        this.instantiate_audio(resource_container, response);
-      }
-      else
-      {
-        resource_container.set_resource(this.resource_id, this.url, resource_container.resources_by_url[this.url]);
+      resource_container.set_resource(this.resource_id, this.url, resource_container.resources_by_url[this.url]);
 
-        this.__update_downloaded_bytes(1, 1);
-        this.__loading_ended();
-      }
+      this.__update_downloaded_bytes(1, 1);
+      this.__loading_ended();
     }
+    // }
   }
 
-  instantiate_audio(resource_container, response)
+  instantiate_audio(resource_container)
   {
-    response.arrayBuffer().then((array_buffer) =>
+    const context = AudioContext.getContext();
+
+    const buffer = context.createBuffer(2, 22050, 44100);
+
+    fetch(this.url).then(r => r.blob()).then((response) =>
     {
-      const context = AudioContext.getContext();
-      context.decodeAudioData(array_buffer, (audio_buffer) =>
+      if (response.arrayBuffer)
+      {
+        response.arrayBuffer().then((array_buffer) =>
+        {
+          context.decodeAudioData(array_buffer, (audio_buffer) =>
+          {
+            resource_container.set_resource(
+              this.resource_id,
+              this.url,
+              new AudioClip(audio_buffer, this.loop, this.volume, this.positional)
+            );
+
+            this.__update_downloaded_bytes(1, 1);
+            this.__loading_ended();
+          });
+        });
+      }
+      else
       {
         resource_container.set_resource(
           this.resource_id,
           this.url,
-          new AudioClip(audio_buffer, this.loop, this.volume, this.positional)
+          new AudioClip(buffer, this.loop, this.volume, this.positional)
         );
 
         this.__update_downloaded_bytes(1, 1);
         this.__loading_ended();
-      });
+      }
     });
   }
 }
