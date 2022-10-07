053c27a101a2db49849df032a6ee7efcf3835d3f
Ramiro <fages.fages@gmail.com>
2022-10-07 03:23:37 -0300

    Fixed camera copy methods

--- src/PerspectiveCamera.js
+++ src/PerspectiveCamera.js
@@ -10,4 +10,12 @@ export default class PerspectiveCamera extends THREEPerspectiveCamera
     this.clear_color = new Color('#000000');
     this.clear_alpha = 1;
   }
+
+  copy(camera)
+  {
+    super.copy(camera);
+    this.clear_color.copy(camera.clear_color);
+    this.clear_alpha = camera.clear_alpha;
+    return this;
+  }
 }
