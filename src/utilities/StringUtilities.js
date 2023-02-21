0560a637a080b9f64cf2b8e1d3df361db7a3e12a
Federico Gratti <federicogratti@gmail.com>
2023-02-21 18:34:57 -0300

    Add get_random_color to StringUtilities

--- src/utilities/StringUtilities.js
+++ src/utilities/StringUtilities.js
@@ -3,6 +3,20 @@ class StringUtilities
   constructor()
   {}
 
+  static get_random_color()
+  {
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hex = '#';
+
+    for (let i = 0; i < 6; i++)
+    {
+      const index = Math.floor(Math.random() * hexValues.length);
+      hex += hexValues[index];
+    }
+
+    return hex;
+  }
+
   static capitalize(string)
   {
     const words = string.split(' ');
--- types/utilities/StringUtilities.d.ts
+++ types/utilities/StringUtilities.d.ts
@@ -1,4 +1,5 @@
 export class StringUtilities {
+    static get_random_color(): string;
     static capitalize(string: any): any;
     static capitalize_first_letter(string: any): any;
     static snake_to_camelcase(string: any): any;
