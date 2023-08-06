7eb8f830446abbbf68fbf4477f87c525605a4738
Federico Gratti <federicogratti@gmail.com>
2023-08-06 12:07:07 -0300

    Added load_iframes and load_elements to HTMLUtilities

--- src/utilities/HTMLUtilities.js
+++ src/utilities/HTMLUtilities.js
@@ -41,6 +41,36 @@ class HTMLUtilities
       }
     }
   }
+
+  static load_iframes(container)
+  {
+    const iframes = container.querySelectorAll('iframe');
+
+    for (let i = 0; i < iframes.length; i++)
+    {
+      const iframe = iframes[i];
+
+      if (iframe.dataset.src)
+      {
+        iframe.src = iframe.dataset.src;
+      }
+    }
+  }
+
+  static load_elements(container, selector)
+  {
+    const elements = container.querySelectorAll(selector);
+
+    for (let i = 0; i < elements.length; i++)
+    {
+      const element = elements[i];
+
+      if (element.dataset.src)
+      {
+        element.src = element.dataset.src;
+      }
+    }
+  }
 }
 
 export { HTMLUtilities };
