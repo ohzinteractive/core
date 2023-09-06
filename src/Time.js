ab6e80301ebf884599a6760f87e32dbea752de00
Ramiro Fages <fages.fages@gmail.com>
2023-09-05 23:24:42 -0300

    Refactored Time. Added smooth_delta_time and fixed_delta_time

--- src/Time.js
+++ src/Time.js
@@ -5,22 +5,33 @@ class Time
 {
   constructor()
   {
-    this.delta_buffer = [0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016];
+    this.__delta_buffer = [0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016, 0.016];
   }
 
   init()
   {
     this.___time = new Clock();
-    this.__raw_delta_time = 0;
+    this.__delta_time = 0;
+    this.__smooth_delta_time = 0;
     this.__elapsed_time = 0;
     this.__allocated_time = new Vector2(0, 0);
-
-    this.delta_time = 0.016;
+    this.__frame_interpolation_t = 0;
+    this.fixed_delta_time = 1 / 30;
   }
 
   get elapsed_time()
   {
     return this.__elapsed_time;
+  }
+
+  get delta_time()
+  {
+    return this.__delta_time;
+  }
+
+  get smooth_delta_time()
+  {
+    return this.__smooth_delta_time;
   }
 
   get shader_time()
@@ -30,27 +41,37 @@ class Time
     return  this.__allocated_time;
   }
 
-  __update()
+  get frame_interpolation()
   {
-    this.__raw_delta_time = this.___time.getDelta();
-    this.__elapsed_time = this.___time.getElapsedTime();
-
-    this.delta_buffer.shift();
-    this.delta_buffer.push(this.__raw_delta_time < 0.32 ? this.__raw_delta_time : 0.032);
-
-    this.__calculate_delta_time();
+    return this.__frame_interpolation_t;
   }
 
-  __calculate_delta_time()
+  __set_frame_interpolation(value)
   {
-    this.delta_time = 0;
+    this.__frame_interpolation_t = value;
+  }
 
-    for (let i = 0; i < this.delta_buffer.length; i++)
+  __update()
+  {
+    this.__delta_time = this.___time.getDelta();
+    this.__elapsed_time = this.___time.getElapsedTime();
+
+    this.__delta_buffer.shift();
+    this.__delta_buffer.push(this.delta_time < 0.32 ? this.delta_time : 0.032);
+
+    this.__calculate_smooth_delta_time();
+  }
+
+  __calculate_smooth_delta_time()
+  {
+    this.__smooth_delta_time = 0;
+
+    for (let i = 0; i < this.__delta_buffer.length; i++)
     {
-      this.delta_time += this.delta_buffer[i];
+      this.__smooth_delta_time += this.__delta_buffer[i];
     }
 
-    this.delta_time = this.delta_time / this.delta_buffer.length;
+    this.__smooth_delta_time = this.__smooth_delta_time / this.__delta_buffer.length;
   }
 }
 
