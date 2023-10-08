2f455fb30bb7127b5548a7f7a76b2390c75b2e0e
Federico Gratti <federicogratti@gmail.com>
2023-10-08 13:13:29 -0300

    Update scene templates

--- tasks/create_scene/template_high_textures.js
+++ tasks/create_scene/template_high_textures.js
@@ -1,9 +1,12 @@
 const template_high_textures = [
   // {
   //   name: 'leaf_texture',
-  //   url: 'textures/leaf.ktx2',
-  //   size: process.env['textures-leaf.ktx2'],
-  //   kind: 'basis' // regular, basis, hdr
+  //   url: 'textures/leaf_texture.png',
+  //   size: process.env['textures-leaf_texture.png'],
+  //   kind: 'regular', // regular, basis, hdr
+  //   flipY: false,
+  //   premultiplyAlpha: false,
+  //   colorSpaceConversion: true
   // }
 ];
 
