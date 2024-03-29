fb910457f44f88bcf62f975cf9a0f72bf984886d
Federico Gratti <federicogratti@gmail.com>
2024-03-29 13:37:36 -0300

    Added create-transition command

--- tasks/create_view/TemplateTransitionController.js
+++ tasks/create_view/TemplateTransitionController.js
@@ -1,7 +1,35 @@
 import { SectionTransitionController } from '../common/SectionTransitionController';
+// import { ViewManager } from 'ohzi-core';
+// import { Sections } from '../Sections';
 
 class TemplateTransitionController extends SectionTransitionController
 {
+  start()
+  {
+    super.start();
+
+    // __CUSTOM_TRANSITIONS__
+  }
+
+  before_enter()
+  {
+    super.before_enter();
+  }
+
+  on_enter()
+  {
+    super.on_enter();
+  }
+
+  update_enter_transition(global_view_data, transition_progress, action_sequencer)
+  {
+    super.update_enter_transition(global_view_data, transition_progress, action_sequencer);
+  }
+
+  update_exit_transition(global_view_data, transition_progress, action_sequencer)
+  {
+    super.update_exit_transition(global_view_data, transition_progress, action_sequencer);
+  }
 }
 
 export { TemplateTransitionController };
